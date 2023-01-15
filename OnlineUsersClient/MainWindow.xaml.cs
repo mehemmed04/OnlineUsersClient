b9bd85917022184206c9efe1b1976e387414b841
Bayr_kf60 <Bayr_kf60@STHQ012D-08>
2023-01-15 17:40:00 +0400

    Add project files.

---
+++ OnlineUsersClient/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OnlineUsersClient.Helper;
+
+namespace OnlineUsersClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            var client = new TcpClient();
+            var ip = IPAddress.Parse(IPHelper.GetLocalIpAddress());
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+
+            try
+            {
+                client.Connect(ep);
+                if (client.Connected)
+                {
+                    var writer = Task.Run(() =>
+                    {
+                        var text = NameTxtb.Text;
+                        var stream = client.GetStream();
+                        var bw = new BinaryWriter(stream);
+                        bw.Write(text);
+                    });
+
+                    //var reader = Task.Run(() =>
+                    //{
+                    //    while (true)
+                    //    {
+                    //        //var stream = client.GetStream();
+                    //        //var br = new BinaryReader(stream);
+                    //        //MessageBox.Show($"FROM Server : {br.ReadString()}");
+                    //    }
+                    //});
+                    //Task.WaitAll();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
